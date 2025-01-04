c2df75d81363ee3ea27edddf89ce1113f4751e44
kia <kiadalili@gmail.com>
2025-01-04 14:24:31 -0500

    Initial Commit

---
+++ EdamesTests/EdamesTests.swift
@@ -0,0 +1,17 @@
+//
+//  EdamesTests.swift
+//  EdamesTests
+//
+//  Created by Kia Dalili on 1/4/25.
+//
+
+import Testing
+@testable import Edames
+
+struct EdamesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
